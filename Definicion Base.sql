58882d0ff622d54f4d37f3bf0acf2d5da267eb87
Dannek Miranda <mdannek@outlook.com>
2019-08-13 00:27:12 -0600

    Update Definicion Base.sql
    Definicion v2

--- Definicion Base.sql
+++ Definicion Base.sql
@@ -1,11 +1,4 @@
-Create Table CURSO(
-
-	id_curso int primary key,
-	nombre varchar(20) not null
-	
-);
-
-Create Table Usuario(
+Create Table USUARIO(
 
 	id_usuario int primary key,
 	nombre_1 varchar(20) not null,
@@ -17,10 +10,42 @@ Create Table Usuario(
 
 );
 
-Create Table Asignacion(
+Create Table CURSO(
+
+	id_curso int primary key,
+	nombre varchar(20) not null,
+	creador int not null,
+	foreign key(creador) references USUARIO(id_usuario);
+	
+);
+
+
+Create Table ASIGNACION(
 
 	id_curso int,
 	id_usuario int,
-	primary key(id_curso,id_usuario)
-
+	primary key(id_curso,id_usuario),
+	foreign key(id_curso) references CURSO(id_curso),
+	foreign key(id_usuario) references USUARIO(id_usuario)
 );
+
+Create Table TAREA(
+	
+	id_tarea int primary key,
+	--campos para preguntas o algo asi
+	Entrega datetime not null,
+	curso int not null,	
+	foreign key(curso) references CURSO(id_curso)
+	
+);
+
+Create Table Nota_Tarea(
+	
+	estudiante int,
+	tarea int,
+	nota int not null default 0,
+	primary key(estudiante,tarea),
+	foreign key(tarea) references TAREA(id_tarea),
+	foreign key(estudiante) references USUARIO(estudiante)
+
+)
