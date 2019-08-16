a59f152e6397619a026e44253f27ca3ba2cb845a
Steve Contreras <stevcont96@gmail.com>
2019-08-15 23:09:11 -0600

    Creacion de cursos
    se han agredado las funciones de crear curso, ver curso por nombre y listar los curso actuales de la base de datos

--- App_Estudios_G7/App_Estudios_G7/Controllers/HomeController.cs
+++ App_Estudios_G7/App_Estudios_G7/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using App_Estudios_G7.Models;
+using App_Estudios_G7.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace App_Estudios_G7.Controllers
 {
     public class HomeController : Controller
     {
+        Sistema_estudiosEntities BD = new Sistema_estudiosEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -26,5 +32,8 @@ namespace App_Estudios_G7.Controllers
 
             return View();
         }
+
+
+
     }
 }
---
+++ App_Estudios_G7/App_Estudios_G7/Models/ViewModels/CursoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace App_Estudios_G7.Models.ViewModels
+{
+    public class CursoViewModel
+    {
+        [Required]
+        [Display(Name ="Nombre")]
+        [StringLength(20)]
+        public string nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Usuario")]
+        public int usuario { get; set; }
+
+    }
+}
