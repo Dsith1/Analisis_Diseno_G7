70882f4dab87a0d322988520de3b601fa839d429
hilbert11 <hilbert.perucho@gmail.com>
2019-08-16 00:23:48 -0600

    Login de Usuario
    Hace falta validar la parte de que tipo de Rol tiene

---
+++ App_Estudios_G7/App_Estudios_G7/Controllers/CuentaController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using App_Estudios_G7.Models;
+using System.Data.SqlClient;
+
+namespace App_Estudios_G7.Controllers
+{
+    public class CuentaController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        // GET: Cuenta
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void conectionString()
+        {
+            con.ConnectionString = "data source=HILBERTPC; database=Sistema_estudios; integrated security = SSPI;";
+        }
+        [HttpPost]
+        public ActionResult Verificar(Cuenta cu)
+        {
+            conectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from USUARIO where nick='"+cu.Name+"' and contra='"+cu.Password+"' ";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return RedirectToAction("Index", "Home");
+            }
+
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+        }
+    }
+}
