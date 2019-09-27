4d6c745f0be26d27bd077b5d13c3d90cd17f805c
Steve Contreras <stevcont96@gmail.com>
2019-09-26 20:05:22 -0600

    Errores del registro corregidos
    se han corregido errores a la hora de registrar un nuevo usuario agregando un combobox al formulario html y corrigiendo el controlador.

--- App_Estudios_G7/App_Estudios_G7.Tests/Controllers/RegistrarseControllerTest.cs
+++ App_Estudios_G7/App_Estudios_G7.Tests/Controllers/RegistrarseControllerTest.cs
@@ -12,16 +12,27 @@ using App_Estudios_G7.Models;
 namespace App_Estudios_G7.Tests.Controllers
 {
     [TestClass]
-    class RegistrarseControllerTest
+    public class RegistrarseControllerTest
     {
         [TestMethod]
-        public void LoginUsuarioNoNull()
+        public void RegistrarUsuarioNoNull()
         {
             // Arrange
             CuentaController controller = new CuentaController();
 
             // Act
-            ViewResult result = controller.Login() as ViewResult;
+            ViewResult result = controller.Registrarse(
+                new USUARIO {
+                    nick ="ni_1",
+                    contra = "asdf",
+                    nombre_1 ="n_1",
+                    nombre_2 ="n_2",
+                    apellido_1 = "a_1",
+                    apellido_2 = "a_2",
+                    edad = 5,
+                    correo = "c@g.com",
+                    rol = "Estudiante"
+                }) as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
