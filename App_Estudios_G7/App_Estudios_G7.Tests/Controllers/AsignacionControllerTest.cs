84ad981d415df490d9321f4b440ba59bb1856013
Cristian David Alvarado Lopez <crisalva5@gmail.com>
2019-09-26 20:39:16 -0600

    Creacion de test
    Paso algo raro pero ya vuelvo a tirar esto

--- App_Estudios_G7/App_Estudios_G7.Tests/Controllers/AsignacionControllerTest.cs
+++ App_Estudios_G7/App_Estudios_G7.Tests/Controllers/AsignacionControllerTest.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App_Estudios_G7;
+using App_Estudios_G7.Controllers;
+using System.Data;
+using App_Estudios_G7.Models;
 
 namespace App_Estudios_G7.Tests.Controllers
 {
     [TestClass]
-    public class AsignarseControllerTest
+    public class AsignacionControllerTest
     {
         [TestMethod]
         public void AsignacionTest()
