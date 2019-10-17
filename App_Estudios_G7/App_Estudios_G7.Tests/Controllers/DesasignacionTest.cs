9c4135e5381f4ea6cb45e5283d768a826e2723f0
Dannek Miranda <mdannek@outlook.com>
2019-10-16 18:28:27 -0600

    TDD1 Desasignacion
    201212763

---
+++ App_Estudios_G7/App_Estudios_G7.Tests/Controllers/DesasignacionTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App_Estudios_G7.Tests.Controllers
+{
+    [TestClass]
+    public class DesasignacionTest
+    {
+        [TestMethod]
+        public void Desasignacion_Falla_Conexion()
+        {
+            Asignaciones asignaciones = new Asignaciones();
+
+            bool exito = asignaciones.Abrir_conn();
+
+            Assert.IsFalse(exito);
+
+        }
+    }
+}
