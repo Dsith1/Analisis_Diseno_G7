aae08182d8ca6a1b698c4b4e59a46b480633f2c1
Dannek Miranda <mdannek@outlook.com>
2019-10-21 12:53:33 -0600

    Tdd Desasignacion3 No Existe Curso
    2012-12763

--- App_Estudios_G7/App_Estudios_G7.Tests/Controllers/DesasignacionTest.cs
+++ App_Estudios_G7/App_Estudios_G7.Tests/Controllers/DesasignacionTest.cs
@@ -32,5 +32,21 @@ namespace App_Estudios_G7.Tests.Controllers
             Assert.IsTrue(exito);
 
         }
+
+        [TestMethod]
+        public void Desasignacion_Fallida_No_Existe_curso()
+        {
+            Asignaciones asignaciones = new Asignaciones();
+
+            asignaciones.cadena = "data source=LAPTOP-IFGR27P8;initial catalog=Sistema_estudios;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+
+            asignaciones.Abrir_conn();
+
+            int curso = 99;
+
+            bool exito = asignaciones.Verificar_curso(curso);
+
+            Assert.IsFalse(exito);
+        }
     }
 }
