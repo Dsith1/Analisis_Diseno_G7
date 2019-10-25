9163340cd6b02227fc5c62ed58a68bdb62930dd5
hilbert11 <hilbert.perucho@gmail.com>
2019-10-24 19:16:37 -0600

    Piramide de Pruebas
    Integracion de pruebas con moq

---
+++ App_Estudios_G7/App_Estudios_G7.Tests/Interface2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Estudios_G7.Tests
+{
+    interface Interface2
+    {
+    }
+    public interface Respuesta
+    {
+        void Respuesta(string message);
+    }
+    public interface Registrarse
+    {
+        bool IsValid(string usuario, string contraseña, string telefono, string correo, string direccion);
+    }
+    public class RegistrarsePrueba
+    {
+        Respuesta _log;
+        Registrarse _registro;
+        public RegistrarsePrueba(Registrarse correcto, Respuesta respuesta)
+        {
+            _registro = correcto;
+        }
+
+
+        private void Log(string message)
+        {
+            if (_log != null)
+                _log.Respuesta(message);
+        }
+
+        public bool EsCorrecto(string name, string pass, string tel, string corr, string direc)
+        {
+            if (_registro.IsValid(name, pass, tel, corr, direc))
+            {
+                Log("Registrado Correctamente");
+                return true;
+            }
+            else
+            {
+                Log("Error al Registro");
+                throw new ArgumentException("No es valido el usuario");
+            }
+        }
+    }
+}
---
+++ App_Estudios_G7/App_Estudios_G7.Tests/moqPrueba.cs
@@ -0,0 +1,35 @@
+﻿using App_Estudios_G7.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Estudios_G7.Tests
+{
+    [TestClass]
+    public class moqPrueba
+    {
+        [TestMethod]
+        public void TestIniciarSesion()
+        {
+            //creamos objetos dummy
+            var mockLog = new Mock<ILog>();
+            var validarSesion = new Mock<Sesion>();
+            BlogService blog = new BlogService(validarSesion.Object, mockLog.Object);
+            Assert.IsNotNull(blog);
+        }
+
+        [TestMethod]
+        public void TestRegistrarse()
+        {
+            //creamos objetos dummy
+            var mockLog = new Mock<Respuesta>();
+            var validarRegistro = new Mock<Registrarse>();
+            RegistrarsePrueba blog = new RegistrarsePrueba(validarRegistro.Object, mockLog.Object);
+            Assert.IsNotNull(blog);
+        }
+    }
+}
